d6fe359fe2e110ac4dfdb499cb621c946f555333
João Fernandes Praxedes Neto <jofernandes47@gmail.com>
2022-04-07 16:20:21 -0300

    WIP: controller Login

---
+++ src/main/java/com/cassinodompedro/Auth/Login.java
@@ -0,0 +1,14 @@
+package com.cassinodompedro.Auth;
+
+public class Login {
+    private String email;
+    private String senha;
+    
+    public String getEmail() {
+        return email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+}
+
