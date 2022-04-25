edb3980d9c0588857a467d7c6ca8768ac210a6a3
João Fernandes Praxedes Neto <jofernandes47@gmail.com>
2022-04-24 23:40:12 -0300

    comments

--- src/main/java/com/cassino/cassinodompredroi/services/Auth.java
+++ src/main/java/com/cassino/cassinodompredroi/services/Auth.java
@@ -1,9 +1,11 @@
 package com.cassino.cassinodompredroi.services;
 
+import com.cassino.cassinodompredroi.models.Cliente;
+
 public class Auth {
     private String login;
     private String senha;
-
+    
 
 
     public Boolean Authenticate(){
@@ -21,5 +23,15 @@ public class Auth {
     }
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+    public Cliente  getClienteLogado(){
+        // O que eu pensei nesse metodo era retornar o cliente logado, esse metodo pesquisaria no banco e retornava o cliente pelo login e senha dele
+        // com esse metodo poderiamos ter uma instancia de um cliente logado, para todas as vezes que alternar entre as paginas
+        // conseguiriamos fazer o controller checar esse cliente para renderizar a pagina de acordo com essa instacia do cliente logado
+
+        // a função ta incompleta mas a ideia é que caso o usuário seja null, a gente meio que 
+        // consegue NÂO retornar as paginas nos controllers, e sempre dar redirect para /login
+        Cliente usuario = null;
+        return usuario;
     }
 }
